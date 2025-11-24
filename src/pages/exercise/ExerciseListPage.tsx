96cd45bbf45e163c3db400981cbbc37a7b6e3ec1
Maksym <mrymnik3d@gmail.com>
2025-11-24 17:34:59 +0200

    Якимсь чином все парцювало а потім виявилося шо тег не закритий, йомай..

--- src/pages/exercise/ExerciseListPage.tsx
+++ src/pages/exercise/ExerciseListPage.tsx
@@ -171,7 +171,7 @@ export default function ExerciseListPage() {
               <Divider />
             </Card>
           ))}
-        </Stack>
+        </Box>
       ) : (
         <Box
           sx={{
